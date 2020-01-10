101b0874909e75f88491d8c8d57d890e212241d3
Aleksandra Neskoromnaya <aleksa.nesko@gmail.com>
2020-01-10 23:21:22 +0300

    first try

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld
+{
+  // arguments are passed using the text field below this editor
+  public static void main(String[] args)
+  {
+  	System.out.print("Hello World");
+  }
+}
+
